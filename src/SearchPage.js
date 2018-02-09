140c148b0bd91c459d063f2a1a4cee45fd110caf
Jeremy S. Lee <jeremysanglee@gmail.com>
2018-02-09 15:41:42 -0800

    feat: Give basic structure and method to SearchPage component

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -2,14 +2,39 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class SearchPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      query: e.target.value
+    });
+  }
+
   render() {
     return (
-      <div>
-        <Link
-          className='back-to-bookshelves'
-          to='/'>
-          Back to bookshelves
-        </Link>
+      <div className='search-books'>
+        <div className='search-books-bar'>
+          <Link
+            className='back-to-bookshelves'
+            to='/'>
+            Back to Bookshelves
+          </Link>
+          <div className='search-books-input'>
+            <input
+              onChange={this.handleChange}
+              type='text'
+              placeholder='Search by title or author'/>
+          </div>
+        </div>
+        <div className='search-books-results'>
+          <ol className='results-grid'></ol>
+        </div>
       </div>
     );
   }
