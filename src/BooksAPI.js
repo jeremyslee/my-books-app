a7e16f8343988aac0616de115d36b6cbab0b1b95
Jeremy S. Lee <jeremysanglee@gmail.com>
2018-02-09 17:14:40 -0800

    feat: Add and export various API methods

--- src/BooksAPI.js
+++ src/BooksAPI.js
@@ -8,3 +8,34 @@ const headers = {
   'Accept': 'application/json',
   'Authorization': token
 };
+
+export const get = (bookId) =>
+  fetch(`${api}/books/${bookId}`, { headers })
+    .then(res => res.json())
+    .then(data => data.book);
+
+export const getAll = () =>
+  fetch(`${api}/books`, { headers })
+    .then(res => res.json())
+    .then(data => data.books);
+
+export const update = (book, shelf) =>
+  fetch(`${api}/books/${book.id}`, {
+    method: 'PUT',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ shelf })
+  }).then(res => res.json());
+
+  export const search = (query) =>
+    fetch(`${api}/search`, {
+      method: 'POST',
+      headers: {
+        ...headers,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ query })
+    }).then(res => res.json())
+      .then(data => data.books);
