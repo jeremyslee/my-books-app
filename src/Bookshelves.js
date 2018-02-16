3345dd1206e233920b9669e0b83c263b639f994e
Jeremy S. Lee <jeremysanglee@gmail.com>
2018-02-15 21:39:02 -0800

    feat: Add bookshelves on the main page

--- src/Bookshelves.js
+++ src/Bookshelves.js
@@ -3,13 +3,47 @@ import { Link } from 'react-router-dom';
 
 class Bookshelves extends Component {
   render() {
+    const { collection } = this.props;
+
+
     return (
       <div>
-        <header className="App-header">
+        {/* <header className="App-header">
           <h1 className="App-title">My Books App</h1>
-        </header>
+        </header> */}
+        
+        <div className='list-books-title'>
+          <h1>Book Collection</h1>
+        </div>
+        <div className='list-books-content'>
+          <div>
+            <div className='bookshelf'>
+              <h2 className='bookshelf-title'>Currently Reading</h2>
+              <div className='bookshelf-books'>
+                <ol className='books-grid'>
+                </ol>
+              </div>
+            </div>
+            <div className='bookshelf'>
+              <h2 className='bookshelf-title'>Want to Read</h2>
+              <div className='bookshelf-books'>
+                <ol className='books-grid'>
+                </ol>
+              </div>
+            </div>
+            <div className='bookshelf'>
+              <h2 className='bookshelf-title'>Read</h2>
+              <div className='bookshelf-books'>
+                <ol className='books-grid'>
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
         <Link
-          className='search-books'
+          className='open-search'
           to='/search'>
           Search Books
         </Link>
