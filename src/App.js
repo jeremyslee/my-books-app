6ce0fe33adf68a02bee9e4add0e348f459d5b130
Jeremy S. Lee <jeremysanglee@gmail.com>
2018-02-15 20:58:50 -0800

    feat: Create state shelvedBooks in App component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
 import SearchPage from './SearchPage';
 import Bookshelves from './Bookshelves';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shelvedBooks: []
+    }
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      const booksReorganized = books.map(book => ({
+        authors: book.authors,
+        averageRating: book.averageRating,
+        categories: book.categories,
+        description: book.description,
+        id: book.id,
+        imageLinks: book.imageLinks,
+        industryIdentifiers: book.industryIdentifiers,
+        pageCount: book.pageCount,
+        printType: book.printType,
+        publishedDate: book.publishedDate,
+        publisher: book.publisher,
+        ratingsCount: book.ratingsCount,
+        shelf: book.shelf,
+        subtitle: book.subtitle,
+        title: book.title,
+      }));
+      this.setState({shelvedBooks: booksReorganized});
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">My Books App</h1>
-        </header>
         <Route exact path='/' render={() => (
           <Bookshelves/>
         )}/>
